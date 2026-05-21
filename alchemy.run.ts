4adc44ced5f8b30ff14066981f0a468988ada7c4
Justin Bennett <git@just-be.dev>
2026-05-21 13:57:30 -0400

    Setup alchemy

---
+++ alchemy.run.ts
@@ -0,0 +1,18 @@
+import * as Alchemy from "alchemy";
+import * as Cloudflare from "alchemy/Cloudflare";
+import * as Effect from "effect/Effect";
+
+export default Alchemy.Stack(
+  "Pal",
+  {
+    providers: Cloudflare.providers(),
+    state: Cloudflare.state(),
+  },
+  Effect.gen(function* () {
+    const bucket = yield* Cloudflare.R2Bucket("Bucket");
+
+    return {
+      bucketName: bucket.bucketName,
+    };
+  }),
+);
